458ba04c586e8434f5d87c9b0bfb5ed0bae90ca9
Raphaël Painter <47798393+RaphaelPainter@users.noreply.github.com>
2023-11-07 13:52:38 +0100

    Added Customer Controller

---
+++ src/main/kotlin/com/rpainter/recepe/api/controllers/CustomerController.kt
@@ -0,0 +1,20 @@
+package com.rpainter.recepe.api.controllers
+
+import com.rpainter.recepe.api.entities.Customer
+import com.rpainter.recepe.api.services.impl.CustomerService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class CustomerController(val customerService: CustomerService) {
+
+    @GetMapping("/customers")
+    fun all(): ResponseEntity<MutableIterable<Customer>> {
+        return ResponseEntity.ok( customerService.all());
+    }
+
+}
