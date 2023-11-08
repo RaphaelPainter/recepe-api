ea0c25859911e6e4134845f0740a6fb440a17715
Raphaël Painter <47798393+RaphaelPainter@users.noreply.github.com>
2023-11-08 11:06:07 +0100

    chores

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,17 +15,19 @@ CREATE TABLE customer (
    id UUID NOT NULL,
    name VARCHAR(50) NOT NULL,
    email VARCHAR,
-   password VARCHAR,
+   encrypted_password VARCHAR,
    creation_date DATE NOT NULL,
    role VARCHAR,
-   unique(name),
    unique(email),
    primary key(id)
 );
-INSERT INTO customer (id, name, creation_date) values (
+INSERT INTO customer (id, name,  email, encrypted_password, creation_date, role) values (
     '550e8400-e29b-41d4-a716-446655440000',
-    'test',
-    '2023-11-07'
+    'Paul',
+    'test@wanadoo.com',
+    '$2a$10$JVWfPunx/.7PDxNjk8XH3eHsw0/eGP8ZiSA4l9FjFfPr.HAKQ6QK.',
+    '2023-11-07',
+    'ADMIN'
 );
 
 CREATE TABLE recipe (
