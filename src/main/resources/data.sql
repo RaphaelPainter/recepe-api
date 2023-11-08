d757dcbb44129d52d27bfc2fcfd12bb071584a6c
Raphaël Painter <47798393+RaphaelPainter@users.noreply.github.com>
2023-11-08 17:36:32 +0100

    chore

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 ALTER TABLE if exists recipe
-DROP CONSTRAINT  IF EXISTS fk_recipe_customer;
-ALTER TABLE if exists customer
-DROP CONSTRAINT IF EXISTS fk_customer_role;
+DROP CONSTRAINT  IF EXISTS fk_recipe_api_user;
+ALTER TABLE if exists api_user
+DROP CONSTRAINT IF EXISTS fk_api_user_role;
 
 
-DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS api_user;
 DROP TABLE IF EXISTS recipe;
 DROP TABLE IF EXISTS role;
 
 
 
 
-CREATE TABLE customer (
+CREATE TABLE api_user (
    id UUID NOT NULL,
    name VARCHAR(50) NOT NULL,
    email VARCHAR,
@@ -21,13 +21,21 @@ CREATE TABLE customer (
    unique(email),
    primary key(id)
 );
-INSERT INTO customer (id, name,  email, encrypted_password, creation_date, role) values (
+INSERT INTO api_user (id, name,  email, encrypted_password, creation_date, role) values (
     '550e8400-e29b-41d4-a716-446655440000',
-    'Paul',
-    'test@wanadoo.com',
+    'admin',
+    'admin',
     '$2a$10$JVWfPunx/.7PDxNjk8XH3eHsw0/eGP8ZiSA4l9FjFfPr.HAKQ6QK.',
     '2023-11-07',
     'ADMIN'
+);
+INSERT INTO api_user (id, name,  email, encrypted_password, creation_date, role) values (
+    '450e8400-e29b-41d4-a716-446655440000',
+    'user',
+    'user',
+    '$2a$10$JVWfPunx/.7PDxNjk8XH3eHsw0/eGP8ZiSA4l9FjFfPr.HAKQ6QK.',
+    '2023-11-07',
+    'USER'
 );
 
 CREATE TABLE recipe (
@@ -38,8 +46,8 @@ CREATE TABLE recipe (
    created_by VARCHAR NOT NULL,
    primary key(id),
    unique(name),
-   CONSTRAINT fk_recipe_customer
+   CONSTRAINT fk_recipe_api_user
        FOREIGN KEY (created_by)
-       REFERENCES customer(id)
+       REFERENCES api_user(id)
 );
 
